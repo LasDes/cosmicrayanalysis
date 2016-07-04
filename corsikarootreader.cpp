2fa48ed2aefe08c175034990135defb5705bff73
birdman550 <sanket550@live.com>
2016-07-04 13:54:28 +0530

    License text added
    Comments about the Mozilla License added

--- corsikarootreader.cpp
+++ corsikarootreader.cpp
@@ -1,6 +1,11 @@
 /*Code to plot traces of muons detected in a single module of GRAPES-3
 **Sanket Deshpande**
 */
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ 
 #include <TBox.h>
 #include <TLine.h>
 #include <stdlib.h>
